ae5efbdfcabf28f881bbe71027badb1377bf0496
Nick Tem <nola234@icloud.com>
2026-04-02 23:29:29 +0200

    refactor: update loader to use OUTPUT_DIR and TOP_NUMBER from config

--- src/loader.py
+++ src/loader.py
@@ -1,14 +1,15 @@
 from openpyxl import load_workbook
+from config import OUTPUT_DIR, TOP_NUMBER
 
-def load(df, country_name):
+def load(df, country_name, OUTPUT_DIR, TOP_NUMBER):
     file_name = country_name.replace(" ", "_")
-    file_path = f"output/{file_name}_top10.xlsx"
+    file_path = f"{OUTPUT_DIR}{file_name}_top{TOP_NUMBER}.xlsx"
     df.to_excel(file_path, index=False)
 
     wb = load_workbook(file_path)
     ws = wb.active
     for row in ws.iter_rows(min_row=2, min_col=5, max_col=5):
         for cell in row:
-            cell.number_format = '#,##0.00'
+            cell.number_format = '#,##0.00 "USD'
     wb.save(file_path)
     
