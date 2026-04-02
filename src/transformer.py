0f566f79baa0cb4e5e5e5fd1ae472c92e1ea916d
Nick Tem <nola234@icloud.com>
2026-04-02 23:25:53 +0200

    refactor: update transformer to use TOP_NUMBER from config

--- src/transformer.py
+++ src/transformer.py
@@ -1,5 +1,7 @@
-def transform(data, country_name):
+from config import TOP_NUMBER
+
+def transform(data, country_name, TOP_NUMBER):
     country = data[data["country"].str.contains(country_name, na=False)].copy()
     country['balance'] = country['box_office'] - country['budget']
-    country_sorted = country.sort_values(by='balance', ascending=False).head(10)
+    country_sorted = country.sort_values(by='balance', ascending=False).head(TOP_NUMBER)
     return country_sorted[["title", "release_year", "genre", "director", "balance"]].copy()
